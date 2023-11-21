754741633d7fecaeb19f83ce8861c49962fba920
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2023-11-21 13:51:19 +0100

    did tutorial

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-// // select your html elements into javascript
-// const image = document.querySelector('img');
-// const input = document.querySelector('input');
+const addImgIcon = document.getElementById("AddImgIcon");
+const fileInput = document.getElementById("fileInput");
+const image = document.getElementById("Image");
 
-
-// // then add an event listener for the input variable such that when the input field changes event / state it should run the function after it 
-// input.addEventListener('change', () => {
-//   const selectedFile = input.files[0];
-//   image.src = URL.createObjectURL(selectedFile)
-//   console.log(selectedFile);
-// });
-
-const image = document.querySelector('img');
-const input = document.querySelector('input');
-
-input.addEventListener('change', () => {
-  const selectedFiles = input.files;
-
-  if (selectedFiles.length > 0) {
-    const imageArray = [];
-
-    for (let i = 0; i < selectedFiles.length; i++) {
-      const fileName = selectedFiles[i].name;
-      console.log('Selected file:', fileName);
-      imageArray.push(URL.createObjectURL(selectedFiles[i]));
-    }
-    console.log(selectedFiles);
-    console.log('Array of image URLs:', imageArray);
-
-    // Display the first image in the array inside the circle
-    image.src = imageArray[0];
-  } else {
-    console.log('No files selected');
-  }
+addImgIcon.addEventListener("click", () => {
+    fileInput.click();
 });
+
+fileInput.addEventListener("change", () => {
+    const selectFile = fileInput.files[0];
+    image.src = URL.createObjectURL(selectFile);
+    console.log(selectFile);
+});
+
+
